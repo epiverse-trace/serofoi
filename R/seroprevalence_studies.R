fc72fa09a5d28b56d0bc15031319f52e7a1e3d15
GeraldineGomez <46997659+GeraldineGomez@users.noreply.github.com>
2022-11-20 20:05:44 -0500

    modify dependecies, document and name functions

--- R/seroprevalence_studies.R
+++ R/seroprevalence_studies.R
@@ -1,3 +1,20 @@
+#' Run Save Models
+#'
+#' Función que corre los modelos guardados
+#' Function that runs saved models
+#' @param my_dir
+#' @param suv
+#' @param data dat0
+#' @param n_iters
+#' @param n_warmup
+#' @param model Model0
+#' @param model Model1
+#' @param model Model2
+#' @param name_model NameModel0
+#' @param name_model NameModel1
+#' @param name_model NameModel2
+#' @return run save models
+#' @export
 run_save_models <- function(my_dir,
                           suv,
                           dat0,
@@ -11,8 +28,8 @@ run_save_models <- function(my_dir,
   my_dir0 <- paste0(config::get("test_files_path"), my_dir)
 
 
-  dat <- filter(dat0, survey == suv) %>% arrange(age_mean_f) %>%
-    mutate(birth_year = tsur - age_mean_f)
+  dat <- dplyr::filter(dat0, .data$survey == suv) %>% dplyr::arrange(.data$age_mean_f) %>%
+    dplyr::mutate(birth_year = .data$tsur - .data$age_mean_f)
 
   mod_0 <- fit_model(model = Model0, dat,
                      m_name = NameModel0, n_iters = n_iters); print(paste0(suv, ' finished ------ Model_0'))
@@ -33,8 +50,8 @@ run_save_models <- function(my_dir,
   PlotsM2    <- generate_combined_plots(res = mod_2, dat = dat, lambda_sim = NA, max_lambda) ; print(paste0(suv, ' finished ------ Plots_M2'))
 
   # ---- Statistical Checking
-  dif_m0_m1 <- loo_compare (mod_0$loo_fit, mod_1$loo_fit)
-  dif_m0_m2 <- loo_compare (mod_0$loo_fit, mod_2$loo_fit)
+  dif_m0_m1 <- loo::loo_compare(mod_0$loo_fit, mod_1$loo_fit)
+  dif_m0_m2 <- loo::loo_compare(mod_0$loo_fit, mod_2$loo_fit)
 
   PlotsM0$summary_mod$difference <- 0; PlotsM0$summary_mod$diff_se <- 1;
   PlotsM1$summary_mod$difference <- dif_m0_m1[1];   PlotsM1$summary_mod$diff_se <- dif_m0_m1[2];
@@ -71,7 +88,7 @@ run_save_models <- function(my_dir,
 
 
 
-  png(name_fitting_plots, width = 2000, height = 2000)
+  grDevices::png(name_fitting_plots, width = 2000, height = 2000)
 
   grid.arrange(parrange_0,
                parrange_1,
@@ -105,6 +122,13 @@ run_save_models <- function(my_dir,
 
 }
 
+#' Dir Results
+#'
+#' Función guarda los resultados en una dirección
+#' Function saves the results in a place
+#' @param name_dir
+#' @return dir
+#' @export
 dir_results <- function(name_dir)
 {
 
--- R/visualization.R
+++ R/visualization.R
@@ -1,79 +1,84 @@
-#' Generate Previous Expanded Plot
+#' Generate Combined Plots
 #'
-#' Función que genera la gráfica previa expandida
-#' Function that generates the previous expanded graphic
+#' Función que genera la gráfica combinada
+#' Function that generates the combined graph
+#' @param res
 #' @param data Data
 #' @param xlabel Label of axis x
 #' @param ylabel Label of axis y
-#' @return The previous expanded graphic
+#' @return The combined plots
 #' @export
-generate_prev_expanded_plot <- function(data, res, xlabel = "Age", ylabel = "Sero-positivity") {
-  foi <- rstan::extract(res$fit, "foi", inc_warmup = FALSE)[[1]]
-  prev_expanded <- get_prev_expanded(foi, data)
-
-  plot_prev <-
-    ggplot2::ggplot(prev_expanded) +
-    ggplot2::geom_ribbon(aes(x = age, ymin = predicted_prev_lower, ymax = predicted_prev_upper), fill = "#c994c7") +
-    ggplot2::geom_line(aes(x = age, y = predicted_prev), colour = "#7a0177") +
-    ggplot2::geom_errorbar(aes(age, ymin = p_obs_bin_l, ymax = p_obs_bin_u), width = 0.1) +
-    ggplot2::geom_point(aes(age, p_obs_bin, size = bin_size), fill = "#7a0177", colour = "black", shape = 21) +
-    ggplot2::theme_bw(size_text) +
-    ggplot2::coord_cartesian(xlim = c(0, 60), ylim = c(0,1)) +
-    ggplot2::theme(legend.position = "none") +
-    ggplot2::ylab(ylabel) + ggplot2::xlab(xlabel)
-
-  return(plot_prev)
-}
-
-generate_foi_plot <- function(foi_data) {
-  foi_data$simulated <- get_length_foi_data(foi_data)
-
-  foi_data$medianv[1] <- NA
-  foi_data$lower[1] <- NA
-  foi_data$upper[1] <- NA
-
-  plot_foi <-
-    ggplot(foi_data) +
-    geom_ribbon(aes( x = year, ymin = lower, ymax = upper), fill = "#41b6c4", alpha = 0.5) +
-    geom_line(aes(x = year, y = medianv), colour = "#253494", size = size_text/8) +
-    theme_bw(size_text) +
-    coord_cartesian(ylim = c(0, max_lambda)) +
-    ylab("Force-of-Infection") + xlab("Year")
-
-  return(plot_foi)
-}
-
-generate_rhats_plot <- function(res, ylabel = "Convergence (R^)") {
-  rhats <- get_table_rhats(res)
-
-  plot_rhats <- ggplot(rhats, aes(year, rhat)) +
-    geom_line(colour = "purple") +
-    geom_point() +
-    coord_cartesian(ylim = c(0.7, 2)) +
-    geom_hline(yintercept = 1.1, colour = "blue", size = size_text/12) +
-    theme_bw(size_text) +
-    ylab(ylabel)
-}
-
 generate_combined_plots <- function(res, dat, lambda_sim = NA, max_lambda = NA, size_text = 25) {
+
   if (is.character(res$fit) == FALSE)  {
-    if (class(res$fit@sim$samples) != "NULL" ) {
+    if  (class(res$fit@sim$samples)  != "NULL" ) {
 
       summary_mod <- extract_summary_mod(res, dat)
-      foi <- rstan::extract(res$fit, "foi", inc_warmup = FALSE)[[1]]
+      foi <- rstan::extract(res$fit, 'foi', inc_warmup = FALSE)[[1]]
 
-      plot_prev <- generate_prev_expanded_plot(data, res)
 
-      foi_data <- res$foi_cent_est
-      plot_foi <- generate_foi_plot(foi_data)
+      prev_expanded <- get_prev_expanded(foi, dat)
+      plot_prev <-
+        ggplot2::ggplot(prev_expanded) +
+        ggplot2::geom_ribbon(ggplot2::aes(x = age, ymin = predicted_prev_lower, ymax = predicted_prev_upper), fill = '#c994c7') +
+        ggplot2::geom_line(ggplot2::aes(x = age, y = predicted_prev), colour = '#7a0177') +
+        ggplot2::geom_errorbar(ggplot2::aes(age, ymin = p_obs_bin_l, ymax = p_obs_bin_u), width = 0.1) +
+        ggplot2::geom_point(ggplot2::aes(age, p_obs_bin, size = bin_size), fill = '#7a0177', colour = 'black', shape = 21) +
+        ggplot2::theme_bw(size_text) +
+        ggplot2::coord_cartesian(xlim = c(0, 60), ylim = c(0,1)) +
+        ggplot2::theme(legend.position = 'none') +
+        ggplot2::ylab('Sero-positivity') + ggplot2::xlab("Age")
 
+
+
+      #-------- This bit is to get the actual length of the foi data
+      foi_dat <- res$foi_cent_est
       if (!is.na(lambda_sim)) {
-        lambda_plot <- plot_foi + geom_line(aes(x = year, y = simulated), colour = "red", size = 1.5)
+        lambda_mod_length <- NROW(foi_dat)
+        lambda_sim_length <- length(lambda_sim)
+
+        if (lambda_mod_length < lambda_sim_length) {
+          remove_x_values <- lambda_sim_length - lambda_mod_length
+          lambda_sim <- lambda_sim[-c(1:remove_x_values)]
+        }
+
+        foi_dat$simulated <- lambda_sim
+
       }
 
-      plot_rhats <- generate_rhats_plot(res)
+      #--------
+      foi_dat$medianv[1] <- NA
+      foi_dat$lower[1] <- NA
+      foi_dat$upper[1] <- NA
 
-      plot_data <- generate_info_table_plot(t(summary_mod), size_text = size_text)
+
+      plot_foi <-
+        ggplot2::ggplot(foi_dat) +
+        ggplot2::geom_ribbon(ggplot2::aes(x = year, ymin = lower, ymax = upper), fill = '#41b6c4', alpha = 0.5) +
+        ggplot2::geom_line(ggplot2::aes(x = year, y = medianv), colour = '#253494', size = size_text/8) +
+        ggplot2::theme_bw(size_text) +
+        ggplot2::coord_cartesian(ylim = c(0, max_lambda)) +
+        ggplot2::ylab('Force-of-Infection') + ggplot2::xlab("Year")
+
+
+
+      if (!is.na(lambda_sim)) {
+        lambda_plot <- plot_foi + ggplot2::geom_line(ggplot2::aes(x = year, y = simulated), colour = 'red', size = 1.5)
+      }
+
+      rhats <- get_table_rhats(res)
+
+      plot_rhats <- ggplot2::ggplot(rhats, ggplot2::aes(year, rhat)) +
+        ggplot2::geom_line(colour = 'purple') +
+        ggplot2::geom_point() +
+        ggplot2::coord_cartesian(ylim = c(0.7, 2)) +
+        ggplot2::geom_hline(yintercept = 1.1, colour = 'blue', size = size_text/12) +
+        ggplot2::theme_bw(size_text) +
+        ggplot2::ylab('Convergence (R^)')
+
+
+      plot_data <- plot_info_table(t(summary_mod), size_text = size_text)
+
 
       plot_arrange <- grid.arrange(plot_data,
                                    plot_prev,
@@ -88,73 +93,100 @@ generate_combined_plots <- function(res, dat, lambda_sim = NA, max_lambda = NA, 
                          summary_mod     = summary_mod,
                          rhats           = rhats,
                          prev_expanded = prev_expanded)
-    }
 
-  } else
+
+
+    } } else
 
     {
-      print("model did not run")
-      print_warning <- "errors"
+      print('model did not run')
+      print_warning <- 'errors'
       df <- data.frame()
 
-      g0 <- ggplot(df) + geom_point() + xlim(0, 10) + ylim(0, 10) +
-        annotate("text", x = 4, y = 5, label = print_warning) +
-        theme_bw(25) +
-        theme(axis.text.x = element_blank(), axis.text.y = element_blank()) +
-        ylab('') + xlab('')
+      g0 <- ggplot2::ggplot(df) + ggplot2::geom_point() + ggplot2::xlim(0, 10) + ggplot2::ylim(0, 10) +
+        ggplot2::annotate("text", x = 4, y = 5, label = print_warning) +
+        ggplot2::theme_bw(25) +
+        ggplot2::theme(axis.text.x = ggplot2::element_blank(), axis.text.y = ggplot2::element_blank()) +
+        ggplot2::ylab('') + ggplot2::xlab('')
       g1 <- g0
-      g0 <- g0 +   labs(subtitle = res$model)+
-        theme(plot.title = element_text(size=10))
+      g0 <- g0 + ggplot2::labs(subtitle = res$model) +
+        ggplot2::theme(plot.title = ggplot2::element_text(size = 10))
 
 
       plots <- grid.arrange(g0, g1, g1, g1, g1, nrow = 5)
       res_p <- list(plots = plots,
-                     loo_fit = c("Not available"),
-                     summary_mod     = c("Not available"),
-                     prev_expanded = c("Not available")
+                     loo_fit = c('Not available'),
+                     summary_mod     = c('Not available'),
+                     prev_expanded = c('Not available')
       )
 
       res_plots <-  list(plots = plots,
                           summary_mod  =   "model did not run")
-  }
+
+
+    }
+
+
 
   return(res_plots)
+
+
 }
 
+#' Get Model Comparison Plot
+#'
+#' Función que obtiene la gráfica de comparación de modelos
+#' Function that obtains the model comparison plot
+#' @param res_comp
+#' @param xlabel Label of axis x
+#' @param ylabel Label of axis y
+#' @return The model comparison graph
+
+#========================= PLOT comparison
 get_model_comparison_plot <- function(res_comp) {
   model_comp  <- res_comp$model_comp
   best_model  <- as.character(res_comp$best_model_data1$model)
   best_modelP <- as.numeric(res_comp$best_model_data1$pvalue)
 
-  emptyp <- ggplot(data = data.frame()) +
-    geom_point() +
-    xlim(0,1) + ylim(0,1) + theme_void() +
-    annotate("text",  x = .5, y = .6, label = best_model, size = 14) +
-    annotate("text",  x = .5, y = .55, label = "(best model)", size = 13)
+  emptyp <-  ggplot2::ggplot(data = data.frame()) +
+    ggplot2::geom_point() +
+    ggplot2::xlim(0,1) +  ggplot2::ylim(0,1) +  ggplot2::theme_void() +
+    ggplot2::annotate('text',  x = .5, y = .6, label = best_model, size = 14) +
+    ggplot2::annotate('text',  x = .5, y = .55, label = '(best model)', size = 13)
 
 
-  infot <- filter(model_comp, converged == "Yes") %>% dplyr::select(model, difference, diff_se, pvalue, best) %>%
-    mutate(diff = round(difference, 2),
+  infot <- dplyr::filter(model_comp, converged == 'Yes') %>% dplyr::select(model, difference, diff_se, pvalue, best) %>%
+    dplyr::mutate(diff = round(difference, 2),
            diff_se = round(diff_se, 2),
            pvalue = round(pvalue, 4))
 
   blank <- data.frame(x = 1:10, y = 1:100)
   table_pars <-
-    ggplot(blank, aes(x, y)) +
-    geom_blank() + ylab("") + xlab("") +
-    annotation_custom(tableGrob(d = infot,
-                                theme = ttheme_default(base_size = 20))) +
-    theme_void(30)
+    ggplot2::ggplot(blank, ggplot2::aes(x, y)) +
+    ggplot2::geom_blank() + ggplot2::ylab('') + ggplot2::xlab('') +
+    ggplot2::annotation_custom(gridExtra::tableGrob(d = infot,
+                                theme = gridExtra::ttheme_default(base_size = 20))) +
+    ggplot2::theme_void(30)
 
-  pf <- plot_grid(emptyp, table_pars, nrow = 2 )
+  pf <- cowplot::plot_grid(emptyp, table_pars, nrow = 2 )
 
   return(pf)
 
 }
 
+#' Get Vertical Plot Arrange per Model
+#'
+#' Función que genera el grafico en un arreglo vertical por modelo
+#' Function that generates the graph in a vertical arrange per model
+#' @param PPC
+#' @param xlabel Label of axis x
+#' @param ylabel Label of axis y
+#' @return The vertical plot arrange per model
+#' @export
+
 vertical_plot_arrange_per_model <- function(PPC){
 
-  pp <- grid.arrange(PPC$plots$plot_data,
+  pp <- gridExtra::grid.arrange(PPC$plots$plot_data,
                      PPC$plots$plot_prev,
                      PPC$plots$plot_foi,
                      PPC$plots$plot_rhats,
@@ -164,16 +196,27 @@ vertical_plot_arrange_per_model <- function(PPC){
 
 }
 
-generate_info_table_plot <- function(info, size_text) {
+#' Generate Info Table Plot
+#'
+#' Función que genera la tabla de información
+#' Function that generates the information table
+#' @param data Data
+#' @param info
+#' @param size_text
+#' @return The previous expanded graphic
+#' @export
+
+plot_info_table <- function(info, size_text){
+
   dato <- data.frame(
     y = NROW(info):1,
-    text = paste0(rownames(info), ": ",info[,1])
+    text = paste0(rownames(info), ': ',info[,1])
   )
-  p <- ggplot(dato, aes(x = 1, y = y)) +
-    scale_y_continuous(limits = c(0, NROW(info) + 1), breaks = NULL) +
+  p <- ggplot2::ggplot(dato, ggplot2::aes(x = 1, y = y)) +
+    ggplot2::scale_y_continuous(limits = c(0, NROW(info) + 1), breaks = NULL) +
     # scale_x_continuous(breaks=NULL) +
-    theme_void() +
-    geom_text(aes(label=text), size = size_text/2.5, fontface = "bold")
+    ggplot2::theme_void() +
+    ggplot2::geom_text(ggplot2::aes(label = text), size = size_text/2.5, fontface = 'bold')
 
   return(p)
 }
