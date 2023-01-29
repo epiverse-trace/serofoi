f5511aaca1e8f6ea2254b362f320c0633dc4d142
zmcucunuba <zulma.cucunuba@javeriana.edu.co>
2023-01-29 13:35:32 -0500

    adding a script for model comparison

--- R/test_comparison.R
+++ R/test_comparison.R
@@ -1,4 +1,4 @@
-remotes::install_github("TRACE-LAC/serofoi", ref = "dev-zulma")
+remotes::install_github("TRACE-LAC/serofoi", ref = "dev-zulma", force = TRUE)
 
 library(serofoi)
 
@@ -6,7 +6,10 @@ data_test <- prepare_data(mydata)
 ?run_model
 
 # Testing model comparison
+#I'm sourcing here because installing from my branch is not working
 
+source("R/model_comparison.R")
+source("R/modelling.R")
 
 model_0 <- run_model(model_data = data_test,
                      model_name = "constant_foi_bi",
