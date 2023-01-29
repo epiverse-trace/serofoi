dae111f37a0781282fa525e1273efb8faead40a1
zmcucunuba <zulma.cucunuba@javeriana.edu.co>
2023-01-29 13:09:41 -0500

    adding info into run-model function

--- R/model_comparison.R
+++ R/model_comparison.R
@@ -1,6 +1,5 @@
 #' Get Table Rhats
 #'
-#' Función que hace la tabla de los rhats
 #' Function that makes the rhats table
 #' @param model_object model_object
 #' @return rhats table
@@ -15,4 +14,19 @@ get_table_rhats <- function(model_object) {
   model_rhats$rhat[model_rhats$rhat == 0] <- NA # This is because I'm not estimating these foi values
 
   return(model_rhats)
+}
+
+
+#' Get Model Comparison
+#'
+#' Function that provides a statisctica comparison between models
+#' @param model_objects_list model_objects to compare
+#' @return compasiron table
+#' @export
+get_comparison_table <- function(model_objects_list) {
+  mod1 <-model_objects_list[1]
+  dif_m0_m1 <- loo_compare (mod_0$loo_fit, mod_1$loo_fit)
+
+
+  return(comparison_table)
 }
