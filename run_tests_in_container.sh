f641ae8b3e62340c84e4f94829e7b00c66f3f3c7
JAIME ANDRÉS PAVLICH MARISCAL <jpavlich@javeriana.edu.co>
2023-02-08 14:02:29 -0500

    small fixes to container

--- run_tests_in_container.sh
+++ run_tests_in_container.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -v rtest-site-library:/root/.R/site-library --mount src="$(pwd)",target="/package" -it rtest
+docker run -v rtest-site-library:/root/.R/site-library --mount src="$(pwd)",target="/package",type=bind -it rtest ./run_tests.sh
