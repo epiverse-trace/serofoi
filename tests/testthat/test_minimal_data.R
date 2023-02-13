efd5ce0ce0876d210743e37ff049d80b9bff2d8e
JAIME ANDRÉS PAVLICH MARISCAL <jpavlich@javeriana.edu.co>
2023-02-13 11:30:01 -0500

    removed erroneous code in unit test

--- tests/testthat/test_minimal_data.R
+++ tests/testthat/test_minimal_data.R
@@ -4,27 +4,27 @@ test_that("minimal data", {
   library(dplyr)
 
   #----- Read Data
-  data_test0 <- readRDS("data/data_bu.RDS")
+  # data_test0 <- readRDS("data/data_bu.RDS")
 
-  age_mean_f <- floor((data_test0$age_min + data_test0$age_max) / 2)
-  age_mean_f == data_test0$age_mean_f
+  # age_mean_f <- floor((data_test0$age_min + data_test0$age_max) / 2)
+  # age_mean_f == data_test0$age_mean_f
 
-  sample_size <- sum(data_test0$total)
-  sample_size == data_test0$sample_size
+  # sample_size <- sum(data_test0$total)
+  # sample_size == data_test0$sample_size
 
-  bin_int <- Hmisc::binconf(data_test0$counts,
-    data_test0$total,
-    alpha = 0.05,
-    method = "exact",
-    return.df = TRUE
-  ) %>%
-    rename(prev_obs = PointEst, prev_obs_lower = Lower, prev_obs_upper = Upper)
+  # bin_int <- Hmisc::binconf(data_test0$counts,
+  #   data_test0$total,
+  #   alpha = 0.05,
+  #   method = "exact",
+  #   return.df = TRUE
+  # ) %>%
+  #   rename(prev_obs = PointEst, prev_obs_lower = Lower, prev_obs_upper = Upper)
 
-  bin_int$prev_obs == data_test0$prev_obs
-  bin_int$prev_obs_lower == data_test0$prev_obs_lower
-  bin_int$prev_obs_upper == data_test0$prev_obs_upper
+  # bin_int$prev_obs == data_test0$prev_obs
+  # bin_int$prev_obs_lower == data_test0$prev_obs_lower
+  # bin_int$prev_obs_upper == data_test0$prev_obs_upper
 
-  data_test <- readRDS("data/data.RDS")
+  data_test <- readRDS(test_path("extdata", "data.RDS"))
   data_test <- prepare_data(data_test, alpha = 0.05)
 
   model_0_object <- run_model(
