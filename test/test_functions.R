0eebf7eff36bbb28bf94dc152872d09a42d034bf
Nicolás Torres Domínguez <45337127+ntorresd@users.noreply.github.com>
2023-02-14 08:33:44 -0500

    Dev doc nicolas (#18)
    * doc: Update documentation for run_model, save_or_load_model and fit_model. The name of some functions and parameters were changed for the sake of clarity (make_yexpo -> get_exposure_years, save_or_read_model -> save_or_load_model, yexpo -> exposure_years, ). The order of functions definitions were in the modeling module were changed; now they appear in hierarchical order starting from run_model, since this is the most important function in the module.
    
    * refac: fit_model_log function removed. Now the function fit_model has an exception that implements the logarithmic model as the removed function used to. fit_model documentation updated. Minor changes in the name of the objects returned by the functions.
    
    * doc: modelling module documentation updated. get_posterior_summary function removed (unused).

---
+++ test/test_functions.R
@@ -0,0 +1,44 @@
+rm(list = ls())
+
+library(devtools)
+library(dplyr)
+
+source("R/modelling.R")
+source("R/seroprevalence_data.R")
+source("R/model_comparison.R")
+source("R/visualization.R")
+mydata <- readRDS("data/data.RDS")
+
+# Modelling module functions
+?prepare_data
+model_data <- prepare_data(model_data = mydata, alpha = 0.05)
+
+?get_exposure_years
+exposure_years <- get_exposure_years(model_data)
+
+?get_exposure_matrix
+exposure_matrix <- get_exposure_matrix(model_data = model_data,
+                                       exposure_years = exposure_years)
+
+?save_or_load_model
+stan_model <- save_or_load_model(model_name = "constant_foi_bi")
+
+?fit_model
+fit_model_test <- fit_model(model_data = model_data,
+                            model_name = "constant_foi_bi",
+                            n_iters = 1000,
+                            n_thin = 2,
+                            delta = 0.90,
+                            m_treed = 10,
+                            decades = 0)
+
+
+?run_model
+model_object <- run_model(model_data = model_data, model_name = "constant_foi_bi")
+
+?extract_model_summary
+model_summary <- extract_model_summary(model_object)
+
+?get_prev_expanded
+foi <- rstan::extract(model_object$fit, "foi", inc_warmup = FALSE)[[1]]
+expanded_prevalence <- get_prev_expanded(foi, model_data)
