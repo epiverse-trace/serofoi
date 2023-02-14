9015ac1a44719a12aae89ee21033f673b64d204b
Hugo Gruson <Bisaloo@users.noreply.github.com>
2023-02-14 12:09:51 +0100

    Add roxygen comments from rstantools::use_rstan()

--- R/serofoi_package.R
+++ R/serofoi_package.R
@@ -4,5 +4,9 @@
 ## usethis namespace: start
 #' @import dplyr
 #' @importFrom dplyr %>%
+#' @import Rcpp
+#' @immport methods
+#' @importFrom rstan sampling
+#' @useDynLib serofoi, .registration=TRUE
 ## usethis namespace: end
 NULL
