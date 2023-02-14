3b395fcf8165b983594160bfacfef1febf14ea3a
zmcucunuba <zulma.cucunuba@javeriana.edu.co>
2023-02-14 10:20:19 -0500

    vignette draft

--- test/test_vignette.R
+++ test/test_vignette.R
@@ -1,4 +1,4 @@
-# library(remotes)
+library(remotes)
 remotes::install_github("TRACE-LAC/serofoi",
                         ref = "dev-zulma-vignette",
                         force = TRUE)
@@ -8,4 +8,7 @@ library(serofoi)
 library(usethis)
 # usethis::use_pkgdown()
 # pkgdown::build_site()
-use_github_pages(branch = "dev-zulma-vignette", path = "/", cname = NA)
+# use_github_pages(branch = "dev-zulma-vignette", path = "/", cname = NA)
+
+library(roxygen2)
+roxygen2::roxygenise()
