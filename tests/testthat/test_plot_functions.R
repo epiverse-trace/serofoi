2fb2b57356b57f3506197091a4c0f00ff766b406
JAIME ANDRÉS PAVLICH MARISCAL <jpavlich@javeriana.edu.co>
2023-02-17 09:48:06 -0500

    Added test functions for plots

--- tests/testthat/test_plot_functions.R
+++ tests/testthat/test_plot_functions.R
@@ -1,6 +1,29 @@
-# TODO complete
 
-test_that("minimal data", {
+
+test_that("plot_seroprev_fitted", {
+  library(devtools)
+  library(dplyr)
+  source("testing_utils.R")
+  set.seed(1234) # For reproducibility
+
+
+  data_test <- prepare_data(mydata)
+
+  model_0_object <- run_model(
+    model_data = data_test,
+    model_name = "constant_foi_bi",
+    n_iters = 1000
+  )
+
+  # model_0_plot <- plot_model(model_0_object, size_text = 6)
+
+  actual_plot <- plot_seroprev_fitted(model_0_object, size_text = 15)
+
+  expect_true(compare_plots("plot_seroprev_fitted", actual_plot))
+})
+
+
+test_that("plot_seroprev", {
   library(devtools)
   library(dplyr)
 
@@ -12,10 +35,9 @@ test_that("minimal data", {
     n_iters = 1000
   )
 
-  model_0_plot <- plot_model(model_0_object, size_text = 6)
+  # model_0_plot <- plot_model(model_0_object, size_text = 6)
 
-  plot_seroprev_fitted(model_0_object, size_text = 15)
-  plot_seroprev(data_test, size_text = 15)
+  actual_plot <- plot_seroprev(data_test, size_text = 15)
 
-  # TODO Complete test ###
+  expect_true(compare_plots("plot_seroprev", actual_plot))
 })
--- tests/testthat/testing_utils.R
+++ tests/testthat/testing_utils.R
@@ -1,4 +1,7 @@
 # TODO Move to separate package ###
+library(testthat)
+library(ggplot2)
+library(png)
 equal_with_tolerance <- function(tolerance = 1e-4) {
     function(a, b, tolerance = 1e-4) {
         x <- base::mapply(function(x, y) abs(x - y), a, b)
@@ -30,4 +33,29 @@ compare_dataframes <- function(df1, df2, column_comparation_functions) {
         }
     }
     return(all_columns_ok)
+}
+
+# withr
+compare_plots <- function(expected_plot_name, actual_plot) {
+    base_path <- test_path("extdata", "plots")
+    actual_plot_filename <- paste(file.path(base_path, "actual", expected_plot_name), "png", sep = ".")
+    print(paste("Saving actual plot image:", actual_plot_filename))
+    ggsave(filename = actual_plot_filename, plot = actual_plot)
+    actual_plot_image <- readPNG(actual_plot_filename)
+
+    expected_plot_filename <- paste(file.path(base_path, "expected", expected_plot_name), "png", sep = ".")
+    if (file.exists(expected_plot_filename)) {
+        expected_plot_image <- readPNG(expected_plot_filename)
+        return(identical(expected_plot_image, actual_plot_image))
+    } else {
+        # If expected plot file does not exist, creates one based on the actual plot image
+        # This behavior is for convenience and should only be used the first time the test is executed,
+        # since it will create the data that will be used as the expected plot in further runs
+        ggsave(filename = expected_plot_filename, plot = actual_plot)
+        warning(
+            "No expected plot image found: ", expected_plot_filename, "\n",
+            "Expected image will be created from the actual plot"
+        )
+        return(TRUE)
+    }
 }
