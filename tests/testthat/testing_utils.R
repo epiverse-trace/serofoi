7a8ebc78766605cf4f27502dfdb7f89b40685887
JAIME ANDRÉS PAVLICH MARISCAL <jpavlich@javeriana.edu.co>
2023-02-21 09:25:20 -0500

    R CMD Check works without errors (locally)

--- tests/testthat/testing_utils.R
+++ tests/testthat/testing_utils.R
@@ -1,7 +1,5 @@
 # TODO Move to separate package ###
 library(testthat)
-library(ggplot2)
-library(png)
 equal_with_tolerance <- function(tolerance = 1e-2) {
     function(a, b) {
         c <- base::mapply(function(x, y) {
@@ -61,35 +59,6 @@ compare_dataframes <- function(expected_df_name, actual_df, column_comparation_f
         )
         dir.create(dirname(expected_df_filename), recursive = TRUE)
         write.csv(actual_df, file = expected_df_filename)
-        return(TRUE)
-    }
-}
-
-
-compare_plots <- function(expected_plot_name, actual_plot) {
-    base_path <- test_path("extdata", "plots") # TODO move to config.yml
-    actual_plot_filename <- paste(file.path(base_path, "actual", expected_plot_name), "png", sep = ".")
-
-    print(paste("Saving actual plot image:", actual_plot_filename))
-    dir.create(dirname(actual_plot_filename), recursive = TRUE)
-    ggsave(filename = actual_plot_filename, plot = actual_plot, width = 5, height = 5, dpi = 300)
-    actual_plot_image <- readPNG(actual_plot_filename)
-
-    expected_plot_filename <- paste(file.path(base_path, "expected", expected_plot_name), "png", sep = ".")
-    if (file.exists(expected_plot_filename)) {
-        expected_plot_image <- readPNG(expected_plot_filename)
-        return(identical(expected_plot_image, actual_plot_image))
-    } else {
-        # If expected plot file does not exist, creates one based on the actual plot image
-        # This behavior is for convenience and should only be used the first time the test is executed,
-        # since it will create the data that will be used as the expected plot in further runs
-        warning(
-            "No expected plot image found: ", expected_plot_filename, "\n",
-            "Expected image will be created from the actual plot"
-        )
-        dir.create(dirname(expected_plot_filename), recursive = TRUE)
-
-        ggsave(filename = expected_plot_filename, plot = actual_plot, width = 5, height = 5, dpi = 300)
         return(TRUE)
     }
 }
