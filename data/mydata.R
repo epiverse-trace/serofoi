d242bae796117533f6d96a23d247c47e613e98a1
ntorresd <ntorresd@unal.edu.co>
2023-02-28 15:47:27 -0500

    add additional changes to add multiple datasets to the package.

--- data/mydata.R
+++ data/mydata.R
@@ -4,12 +4,12 @@
 #'
 #' @docType data
 #'
-#' @usage serodata
+#' @usage mydata
 #'
 #' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
 #'
 #' @keywords datasets
 #'
 #' @examples
-#' serodata
+#' mydata
 "serofoi"
