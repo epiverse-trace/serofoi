f8675129ddeb24e9912a4cab253693a4021d69a4
ntorresd <ntorresd@unal.edu.co>
2023-03-01 11:14:03 -0500

    doc: add mydata and serodata documentation and .R files.

---
+++ R/mydata.R
@@ -0,0 +1,15 @@
+#' Seroprevalence data on serofoi
+#'
+#' Data from a serological surveys
+#'
+#' @docType data
+#'
+#' @usage mydata
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' mydata
+"mydata"
