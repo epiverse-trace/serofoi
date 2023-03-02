590b26526d0e8ccace7c24d48af9f79815ddb296
ntorresd <ntorresd@unal.edu.co>
2023-03-02 13:05:24 -0500

    style: mydata changed to serodata. The current dataset is chagas2012.RDS, but this will be changed to a simulated dataset in the future.

--- R/serodata.R
+++ R/serodata.R
@@ -4,12 +4,12 @@
 #'
 #' @docType data
 #'
-#' @usage mydata
+#' @usage serodata
 #'
 #' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
 #'
 #' @keywords datasets
 #'
 #' @examples
-#' mydata
-"mydata"
+#' serodata
+"serodata"
