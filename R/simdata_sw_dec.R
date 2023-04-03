83db5c1884ab620487efc6c01819c7796a61e93b
ntorresd <ntorresd@unal.edu.co>
2023-04-02 22:03:59 -0500

    add the simulated datasets simdata_constant, simdata_sw_dec and simdata_large_epi that will be used in the vignettes' article 'FoI models'

---
+++ R/simdata_sw_dec.R
@@ -0,0 +1,18 @@
+#' Step-wise decreasing FoI simulated serosurvey
+#'
+#' Simulated dataset describing a situation where the Force-of-Infection
+#' follows a step-wise decreasing tendency over a period of 50 years (2000-2050)
+#' The hypothetical serosurvey is conducted in year 2050 for 250 individuals
+#' that are up to 50 years old.
+#'
+#' @docType data
+#'
+#' @usage simdata_sw_dec
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' simdata_sw_dec
+"simdata_sw_dec"
