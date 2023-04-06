4c16cd542736af28326d77e37426c02afac712a0
ntorresd <ntorresd@unal.edu.co>
2023-04-06 12:01:16 -0500

    remove skip_on_ci() from all tests

--- tests/testthat/test_plot_foi.R
+++ tests/testthat/test_plot_foi.R
@@ -2,7 +2,6 @@ test_that("individual models", {
   # So far we are skipping tests on these platforms until
   # we find an efficient way to update rstan testthat snapshots on all of them
   skip_on_os(c("windows", "mac"))
-  skip_on_ci()
   source("testing_utils.R")
   set.seed(1234) # For reproducibility
 
