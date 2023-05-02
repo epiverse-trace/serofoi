e984f612be3db28efd5961863b6e1af89f96679a
ntorresd <ntorresd@unal.edu.co>
2023-05-01 23:31:42 -0500

    test for the modelling module now compares the results with a standardize dataframe of the expanded prevalence which contains significant information about the seroprevalence fitting

--- tests/testthat/test_modelling.R
+++ tests/testthat/test_modelling.R
@@ -14,59 +14,48 @@ test_that("individual models", {
   data("serodata")
   data_test <- serodata %>% prepare_serodata(alpha = 0.05)
 
+  data_constant_path <- testthat::test_path("extdata", "prev_expanded_constant.RDS")
+  data_tv_normal_path <- testthat::test_path("extdata", "prev_expanded_tv_normal.RDS")
+  data_tv_normal_log_path <- testthat::test_path("extdata", "prev_expanded_tv_normal_log.RDS")
+
+  prev_expanded_tv_normal_log <- readRDS(data_constant_path)
+
   #----- Test for the constant model
 
   model_name <- "constant"
-  model <- run_seromodel(serodata = data_test,
-                         foi_model = model_name,
-                         n_iters = 1000,
-                         print_summary = FALSE)
-  model_plot <- plot_seromodel(model, size_text = 6)
-  vdiffr::expect_doppelganger(paste0(model_name, "_model_plot"), model_plot)
+  model_object <- run_seromodel(serodata = data_test,
+                                foi_model = model_name,
+                                n_iters = 1000,
+                                print_summary = FALSE)
 
-  model_seroprev_plot <- plot_seroprev_fitted(model, size_text = 15)
-  vdiffr::expect_doppelganger(paste0(model_name, "_sp_fitted_plot"), model_seroprev_plot)
+  foi <- rstan::extract(model_object$fit, "foi", inc_warmup = FALSE)[[1]]
+  prev_expanded <- get_prev_expanded(foi, serodata = model_object$serodata)
+  prev_expanded_constant <- readRDS(data_constant_path)
 
-  model_foi_plot <- plot_foi(model, size_text = 15)
-  vdiffr::expect_doppelganger(paste0(model_name, "_foi_plot"), model_foi_plot)
-
-  model_rhats_plot <- plot_rhats(model, size_text = 15)
-  vdiffr::expect_doppelganger(paste0(model_name, "_rhats_plot"), model_rhats_plot)
+  testthat::expect_equal(prev_expanded, prev_expanded_constant, tolerance = TRUE)
 
   #----- Test for the tv_normal model
 
   model_name <- "tv_normal"
-  model <- run_seromodel(serodata = data_test,
-                         foi_model = model_name,
-                         n_iters = 1000)
-  model_plot <- plot_seromodel(model, size_text = 6)
-  vdiffr::expect_doppelganger(paste0(model_name, "_model_plot"), model_plot)
+  model_object <- run_seromodel(serodata = data_test,
+                                foi_model = model_name,
+                                n_iters = 1000)
 
-  model_seroprev_plot <- plot_seroprev_fitted(model, size_text = 15)
-  vdiffr::expect_doppelganger(paste0(model_name, "_sp_fitted_plot"), model_seroprev_plot)
+  foi <- rstan::extract(model_object$fit, "foi", inc_warmup = FALSE)[[1]]
+  prev_expanded <- get_prev_expanded(foi, serodata = model_object$serodata)
+  prev_expanded_tv_normal <- readRDS(data_tv_normal_path)
+  testthat::expect_equal(prev_expanded, prev_expanded_tv_normal, tolerance = TRUE)
 
-  model_foi_plot <- plot_foi(model, size_text = 15)
-  vdiffr::expect_doppelganger(paste0(model_name, "_foi_plot"), model_foi_plot)
-
-  model_rhats_plot <- plot_rhats(model, size_text = 15)
-  vdiffr::expect_doppelganger(paste0(model_name, "_rhats_plot"), model_rhats_plot)
-
-  #----- Generate plots for the constant model
+  #----- Test for the tv_normal_log model
 
   model_name <- "tv_normal_log"
-  model <- run_seromodel(serodata = data_test,
-                         foi_model = model_name,
-                         n_iters = 1000)
-  model_plot <- plot_seromodel(model, size_text = 6)
-  vdiffr::expect_doppelganger(paste0(model_name, "_model_plot"), model_plot)
+  model_object <- run_seromodel(serodata = data_test,
+                                foi_model = model_name,
+                                n_iters = 1000)
 
-  model_seroprev_plot <- plot_seroprev_fitted(model, size_text = 15)
-  vdiffr::expect_doppelganger(paste0(model_name, "_sp_fitted_plot"), model_seroprev_plot)
-
-  model_foi_plot <- plot_foi(model, size_text = 15)
-  vdiffr::expect_doppelganger(paste0(model_name, "_foi_plot"), model_foi_plot)
-
-  model_rhats_plot <- plot_rhats(model, size_text = 15)
-  vdiffr::expect_doppelganger(paste0(model_name, "_rhats_plot"), model_rhats_plot)
+  foi <- rstan::extract(model_object$fit, "foi", inc_warmup = FALSE)[[1]]
+  prev_expanded <- get_prev_expanded(foi, serodata = model_object$serodata)
+  prev_expanded_tv_normal <- readRDS(data_tv_normal_path)
+  testthat::expect_equal(prev_expanded, prev_expanded_tv_normal_log, tolerance = TRUE)
 
 })
