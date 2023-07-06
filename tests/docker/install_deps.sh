bbc6d48076b16facee366f8a41a06ffb94ed0735
jamesaazam <james.azam@lshtm.ac.uk>
2023-07-06 11:40:07 -0500

    deleted docker folder in tests directory (Closes #86)

--- tests/docker/install_deps.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-Rscript -e 'install.packages("testthat", force=FALSE)'
-Rscript -e 'devtools::install_deps(upgrade="always")'
--- tests/docker/run_tests.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-R --no-echo --no-restore -e 'devtools::test()'
