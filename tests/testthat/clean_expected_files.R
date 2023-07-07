9a4a0db0da86f992d4104c35844974370a94d188
jamesaazam <james.azam@lshtm.ac.uk>
2023-07-07 18:20:26 -0500

    Added a nolint tag to preventing linting of a necessary long line

--- tests/testthat/clean_expected_files.R
+++ tests/testthat/clean_expected_files.R
@@ -1,7 +1,7 @@
 # This script removes the svg and csv files used in the automatic tests
 # This is usually required when a new version of rstan is released
 # Random number generation changes for each new version of
-# Rstan, https://mc-stan.org/docs/2_18/reference-manual/reproducibility-chapter.html
+# Rstan, https://mc-stan.org/docs/2_18/reference-manual/reproducibility-chapter.html #nolint
 # thus all files with expected results (both tables and graphs) become
 # obsolete and need to be regenerated
 paths <- c(test_path("_snaps", "*"), test_path("testdata", "dataframes",
