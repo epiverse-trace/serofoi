ae2e715fd200322fa6f48ffd7572a75588e5cbf5
ntorresd <ntorresd@unal.edu.co>
2023-08-15 11:00:30 -0500

    fix(tests): change storing format of  model object files from .json to .RDS and updates test_visualization accordingly

--- tests/testthat/models_serialization.R
+++ tests/testthat/models_serialization.R
@@ -1,5 +1,4 @@
 library(devtools)
-library(dplyr)
 library(serofoi)
 library(testthat)
 
@@ -7,7 +6,7 @@ set.seed(1234) # For reproducibility
 
 #----- Read and prepare data
 data("simdata_large_epi")
-simdata <- simdata_large_epi %>% prepare_serodata()
+simdata <- prepare_serodata(simdata_large_epi)
 no_transm <- 0.0000000001
 big_outbreak   <- 1.5
 foi_sim <- c(rep(no_transm, 32), rep(big_outbreak, 3), rep(no_transm, 15)) # 1 epidemics
@@ -23,11 +22,12 @@ models_list <- lapply(models_to_run,
                       serodata = simdata,
                       n_iters = 1000)
 
-model_constant_json <- jsonlite::serializeJSON(models_list[[1]])
-write_json(model_constant_json, testthat::test_path("extdata", "model_constant.json"))
+saveRDS(models_list[[1]],
+        testthat::test_path("extdata", "model_constant.RDS"))
 
-model_tv_normal_json <- jsonlite::serializeJSON(models_list[[2]])
-write_json(model_tv_normal_json, testthat::test_path("extdata", "model_tv_normal.json"))
+saveRDS(models_list[[2]],
+        testthat::test_path("extdata", "model_tv_normal.RDS"))
 
-model_tv_normal_log_json <- jsonlite::serializeJSON(models_list[[3]])
-write_json(model_tv_normal_json, testthat::test_path("extdata", "model_tv_normal_log.json"))
+saveRDS(models_list[[3]],
+        testthat::test_path("extdata", "model_tv_normal_log.RDS"))
+
