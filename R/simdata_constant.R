b261b43e29e199a256daede3c575a0bf70723800
ntorresd <ntorresd@unal.edu.co>
2023-10-09 17:15:30 -0500

    doc(fix): correct simdata_constant foi value

--- R/simdata_constant.R
+++ R/simdata_constant.R
@@ -1,7 +1,7 @@
 #' Constant FoI simulated serosurvey
 #'
 #' Simulated dataset describing a endemic situation where the Force-of-Infection
-#' is constant over a period of 50 years (2000-2050) with a value of 0.2.
+#' is constant over a period of 50 years (2000-2050) with a value of 0.02.
 #' The hypothetical serosurvey is conducted in year 2050 for 250 individuals
 #' that are up to 50 years old.
 #'
