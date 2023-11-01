f5b5be637e890e87cae3547a1f7b21142f0c5d6f
ntorresd <ntorresd@unal.edu.co>
2023-10-31 19:52:41 -0500

    test: add analytical test for constant foi
    Add analytical test for constant foi for large sample sizes.

--- tests/testthat/test_sim_data.R
+++ tests/testthat/test_sim_data.R
@@ -1,47 +1,34 @@
 test_that("simulated data", {
-
     library(dplyr)
     library(serofoi)
 
     seed <- 1234
     n_iters <- 1000
-    sample_size_by_age <- 5
+    sample_size_by_age <- 10^7
     tsur <- 2050
     birth_year_min <- 2000
 
     #----- Test for constant FoI
-    # case_label <- "constant_foi_"
-    # foi_model <- "constant"
-    # foi_sim <- rep(0.02, tsur - birth_year_min)
-    # max_lambda <- 0.035
-
-    #----- Test for stepwise decreasing FoI
-    case_label <- "smth_dec_foi_" #Smooth-decendent FoI
-    foi_model = "tv_normal"
-    foi_max = 0.2
-    stretch = 0.15
-    x <- 1:(tsur - birth_year_min)
-    foi_sim <- (-foi_max * (atan(stretch * (x - 25))) / (0.5 * pi) + foi_max) / 2
-    max_lambda <- 0.3
-
+    case_label <- "constant_foi_"
+    foi_model <- "constant"
+    max_lambda <- 0.035
     #----- Test function generate_sim_data
-    sim_data <- generate_sim_data(foi = foi_sim,
-                                  sample_size_by_age = sample_size_by_age,
-                                  tsur = tsur,
-                                  birth_year_min = birth_year_min,
-                                  survey_label = 'foi_sim',
-                                  seed = seed)
-    # Check sim_data structure
-    expect_s3_class(sim_data, "data.frame")
-    expect_length(sim_data$birth_year, tsur - birth_year_min)
+    foi_values <- c(0.001, 0.01, 0.1, 0.3, 0.4)
+    for(foi_value in foi_values) {
+      foi_sim <- rep(foi_value, tsur - birth_year_min)
 
-    # Check consistency between sim_foi and the fitted foi
-    sim_seromodel <- run_seromodel(sim_data, foi_model = foi_model, n_iters = n_iters)
-    foi <- rstan::extract(sim_seromodel$seromodel_fit, "foi", inc_warmup = FALSE)[[1]]
-    foi_lower <- apply(foi, 2, function(x) quantile(x, 0.05))
-    foi_upper <- apply(foi, 2, function(x) quantile(x, 0.95))
+      sim_data <- generate_sim_data(foi = foi_sim,
+                                    sample_size_by_age = sample_size_by_age,
+                                    tsur = tsur,
+                                    birth_year_min = birth_year_min,
+                                    survey_label = 'foi_sim',
+                                    seed = seed)
+      prev_exact <- 1 - exp(- foi_value * sim_data$age_mean_f)
 
-    expect_true(all((foi_sim >= foi_lower) & (foi_sim <= foi_upper)))
+      expect_s3_class(sim_data, "data.frame")
+      expect_length(sim_data$birth_year, tsur - birth_year_min)
+      expect_equal(sim_data$prev_obs, prev_exact, tolerance = TRUE)
+    }
 
     #----- Test function group_sim_data
     sim_data <- sim_data %>% mutate(age_min = age_mean_f, age_max = age_mean_f)
