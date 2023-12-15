9a933e987ae91f5d25df886062f4359364c239aa
ntorresd <ntorresd@unal.edu.co>
2023-12-14 22:49:11 -0500

    fix: add importFrom rstantools rstan_config to serofoi-package.R

--- R/serofoi-package.R
+++ R/serofoi-package.R
@@ -15,6 +15,7 @@
 #' @importFrom graphics text
 #' @importFrom utils read.table
 #' @importFrom stats quantile
+#' @importFrom rstantools rstan_config
 #' @references Stan Development Team (NA). RStan: the R interface to Stan. R
 #' package version 2.26.22. https://mc-stan.org
 #'
