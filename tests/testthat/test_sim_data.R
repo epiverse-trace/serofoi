db360044e4c66d18e66d45c83716764da74d7916
ntorresd <ntorresd@unal.edu.co>
2023-12-21 12:36:04 -0500

    test: correct test_sim_data according to changes in data simulation functions

--- tests/testthat/test_sim_data.R
+++ tests/testthat/test_sim_data.R
@@ -3,24 +3,31 @@ test_that("simulated data", {
   library(serofoi)
 
   seed <- 1234
-  sample_size_by_age <- 10^7
   tsur <- 2050
   birth_year_min <- 2000
+  n_years <- tsur - birth_year_min
+  sample_size_by_age <- rep(10^7, n_years)
+
 
   #----- Test for constant FoI
 
   foi_values <- c(0.001, 0.01, 0.1, 0.3, 0.4)
   for (foi_value in foi_values) {
+    sim_data <- data.frame(
+      age_mean_f = seq(1,n_years),
+      tsur = tsur
+    )
     foi_sim <- rep(foi_value, tsur - birth_year_min)
     #----- Test function generate_sim_data
     sim_data <- generate_sim_data(
+      sim_data = sim_data,
       foi = foi_sim,
       sample_size_by_age = sample_size_by_age,
-      tsur = tsur,
-      birth_year_min = birth_year_min,
       survey_label = "foi_sim_constant",
       seed = seed
-    )
+    ) %>%
+    prepare_serodata()
+
     prev_exact <- 1 - exp(-foi_value * sim_data$age_mean_f)
 
     expect_s3_class(sim_data, "data.frame")
@@ -35,17 +42,22 @@ test_that("simulated data", {
   }
 
   #----- Test for time-varying FoI
+  sim_data <- data.frame(
+    age_mean_f = seq(1,n_years),
+    tsur = tsur
+  )
+
   no_transm <- 0.0000000001
   foi_sim <- c(rep(0.2, 25), rep(0.1, 10), rep(no_transm, 15))
 
   sim_data <- generate_sim_data(
+    sim_data = sim_data,
     foi = foi_sim,
     sample_size_by_age = sample_size_by_age,
-    tsur = tsur,
-    birth_year_min = birth_year_min,
     survey_label = "sw_dec_foi",
     seed = seed
-  )
+  ) %>%
+  prepare_serodata()
 
   prev_exact <- 1 - exp(-cumsum(rev(foi_sim)))
 
