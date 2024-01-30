e081dff432fd9bcc4d20e570de9dc27d182a30fa
ntorresd <ntorresd@unal.edu.co>
2024-01-29 21:43:48 -0500

    test!: change models testing strategy
    - Replace incorrect use of `testthat::expect_equal()` for correct
    comparison of `get_prev_expanded_clean` outputs with corresponding
    benchmarks.
    - Split modelling testing by type of models (constant and time-varying)

---
+++ tests/testthat/test_constant_model.R
@@ -0,0 +1,44 @@
+# load and prepare data
+data(chagas2012)
+serodata <- prepare_serodata(chagas2012, alpha = 0.05)
+
+test_that("Check cohort ages", {
+  cohort_ages <- get_cohort_ages(serodata = serodata)
+  testthat::expect_equal(nrow(cohort_ages), max(unique(serodata$tsur)) - min(serodata$birth_year))
+})
+
+test_that("Test constant model", {
+  model_name <- "constant"
+  # read benchmark data
+  data_compare_path <- testthat::test_path(
+    "extdata",
+    paste0("prev_expanded_", model_name, ".RDS")
+    )
+  prev_expanded_compare <- readRDS(data_compare_path) %>%
+    mutate(
+      # calculates tolerance as half the confidence interval size
+      tol = (predicted_prev_upper - predicted_prev_lower)/2
+    )
+
+  # run constant model
+  model_object <- run_seromodel(
+    serodata = serodata,
+    foi_model = model_name,
+    n_iters = 1000,
+    print_summary = FALSE
+  )
+
+  foi <- rstan::extract(model_object, "foi", inc_warmup = FALSE)[[1]]
+  prev_expanded <- get_prev_expanded(foi, serodata = serodata)
+
+  # compares expanded prevalence with benchmark
+  expect_true(
+    all(
+      dplyr::near(
+        prev_expanded$predicted_prev,
+        prev_expanded_compare$predicted_prev,
+        tol = prev_expanded_compare$tol
+      )
+    )
+  )
+})
