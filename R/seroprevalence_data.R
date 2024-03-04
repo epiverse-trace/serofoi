52aac83eb0fc0f3366e4c889d93d810bff3d261f
ntorresd <ntorresd@unal.edu.co>
2024-03-04 10:05:09 -0500

    feat: add `probability_exact_time_varying` and its documentation

--- R/seroprevalence_data.R
+++ R/seroprevalence_data.R
@@ -150,6 +150,30 @@ prepare_bin_data <- function(serodata,
 #' Computes the probability of being seropositive for age-varying
 #' FoI including seroreversion
 #'
+#' @param ages Integer indicating the ages of the exposed cohorts
+#' @param foi Numeric atomic vector corresponding to the age-varying FoI to
+#' simulate from
+#' @param mu Seroreversion rate
+#' @return probability of being seropositive for age-varying FoI
+#' including seroreversion
+probability_exact_time_varying <- function(
+    ages,
+    foi,
+    mu = 0
+) {
+  n_ages <- length(ages)
+  exposure_matrix <- matrix(0, nrow = n_ages, ncol = n_ages)
+  for (k in 1:n_ages) {
+    exposure_matrix[k, (n_ages - ages[k] + 1):n_ages] <- 1
+  }
+  probabilities <-
+    (foi / (foi + mu)) * (1 - exp(-drop(exposure_matrix %*% (foi + mu))))
+  return(probabilities)
+}
+
+#' Returns the probability of being seropositive for age-varying
+#' force-of-infection including seroreversion
+#'
 #' @param age Integer corresponding to the age of the exposed cohort
 #' @param foi Numeric atomic vector corresponding to the age-varying FoI to
 #' simulate from
@@ -219,9 +243,11 @@ stopifnot(
   ages <- rev(cohort_ages$age)
 
   if (model_type == "time-varying") {
-    exposure_matrix <- get_exposure_matrix(sim_data) # nolint: object_usage_linter
-    probabilities <-
-    (foi / (foi + mu)) * (1 - exp(-drop(exposure_matrix %*% (foi + mu))))
+    probabilities <- probability_exact_time_varying(
+      ages = ages,
+      foi = foi,
+      mu = mu
+      )
   }
   if (model_type == "age-varying") {
     probabilities <- purrr::map_dbl(
