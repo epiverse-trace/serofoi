119f19d63f4a4c7ebdb63aba7e8b80d20f04a9d5
ntorresd <ntorresd@unal.edu.co>
2024-05-24 19:50:09 -0500

    feat: add working version of `fit_seromodel` using functional specification of priors

---
+++ R/fit_seromodel.R
@@ -0,0 +1,48 @@
+add_age_group_to_serosurvey <- function(serosurvey) {
+  if (!any(colnames(serosurvey) == "age_group")) {
+    serosurvey <- serosurvey %>%
+      dplyr::mutate(
+        age_group = floor((.data$age_min + .data$age_max) / 2)
+      )
+  } else {
+    message("Using `age_group` already present in serosurvey")
+  }
+  return(serosurvey)
+}
+
+fit_seromodel <- function(
+  serosurvey,
+  model_type = "constant",
+  foi_prior = sf_normal(),
+  is_seroreversion = FALSE,
+  seroreversion_prior = sf_uniform(),
+  ...
+) {
+  serosurvey <- serosurvey %>%
+    validate_serosurvey() %>%
+    add_age_group_to_serosurvey()
+
+  stan_data <- build_stan_data(
+    serosurvey = serosurvey,
+    model_type = model_type,
+    foi_prior = foi_prior,
+    is_seroreversion = is_seroreversion,
+    seroreversion_prior = seroreversion_prior
+  )
+
+  if (is_seroreversion)
+    model_name <- paste0(model_type, "_seroreversion")
+  else
+    model_name <- model_type
+
+  # model <- stan_models[[model_name]]
+  model <- rstan::stan_model(paste0("inst/stan/", model_name, ".stan"))
+
+  seromodel <- rstan::sampling(
+    model,
+    data = stan_data,
+    ...
+  )
+
+  return(seromodel)
+}
