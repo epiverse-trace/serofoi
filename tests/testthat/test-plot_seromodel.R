fa5a36a0ea180bb24f20d9c62f6a0de8fac37b88
Jaime A. Pavlich-Mariscal <jaime.pavlich@gmail.com>
2024-10-04 17:09:32 -0500

    added tests for `plot_seromodel` and `plot_summary`

--- tests/testthat/test-plot_seromodel.R
+++ tests/testthat/test-plot_seromodel.R
@@ -3,120 +3,186 @@ library(ggplot2)
 library(rlang)
 library(purrr)
 
-extract_plot_data <- function(plot) {
-  list(
-    classes = class(plot),
-    layers = extract_layers(plot$layers),
-    coordinates = list(
-      limits = plot$coordinates$limits
-    ),
-    labels = plot$labels
-  )
-}
+# NOTE: If the variable `expected_plot` needs to be updated because of changes in the plots
+# You can run dput(actual_plot) to obtain the current structure of the plot
 
-extract_layers <- function(layers) {
-  map(layers, function(layer) {
-    # Extract mappings
-    list(
-      mapping =
-        map(layer$mapping, function(x) {
-          quo_name(get_expr(x))
-        })
-    )
-  })
-}
+# Common data
+data(veev2012)
+serosurvey <- veev2012
 
+seromodel <- fit_seromodel(
+  serosurvey = serosurvey
+)
 
-create_expected_serosurvey <- function(actual_serosurvey) {
+create_prepared_serosurvey <- function(actual_serosurvey) {
   return(prepare_serosurvey_for_plotting(actual_serosurvey %>%
     add_age_group_to_serosurvey()))
 }
 
 
 test_that("plot_serosurvey creates a ggplot with correct structure", {
-  data(veev2012)
+  actual_plot <- extract_plot_data(plot_serosurvey(serosurvey))
 
-  actual_serosurvey <- veev2012
-
-  actual_plot <- extract_plot_data(plot_serosurvey(actual_serosurvey))
-
-  expected_serosurvey <- create_expected_serosurvey(actual_serosurvey)
+  prepared_serosurvey <- create_prepared_serosurvey(serosurvey)
 
   expected_plot <- list(
     classes = c("gg", "ggplot"),
     layers = list(
       list(
         mapping = list(
-          ymin = "seroprev_lower",
-          ymax = "seroprev_upper"
+          ymin = "seroprev_lower", ymax = "seroprev_upper"
+        ), geom_params = list(
+          na.rm = FALSE, orientation = NA, width = 0.1
         )
       ),
       list(
         mapping = list(
-          y = "seroprev",
-          size = "n_sample"
-        )
+          y = "seroprev", size = "n_sample"
+        ), geom_params = list(na.rm = FALSE)
       )
     ),
-    coordinates = list(
-      limits = list(
-        x = c(min(expected_serosurvey$age_min), max(expected_serosurvey$age_max)),
-        y = c(min(expected_serosurvey$seroprev_lower), max(expected_serosurvey$seroprev_upper))
-      )
-    ),
+    coordinates = list(limits = list(x = c(1, 60), y = c(
+      0.3,
+      1
+    ))),
     labels = list(
-      x = "Age",
-      y = "Seroprevalence",
-      ymin = "seroprev_lower",
-      ymax = "seroprev_upper",
-      size = "n_sample"
+      x = "Age", y = "Seroprevalence", ymin = "seroprev_lower",
+      ymax = "seroprev_upper", size = "n_sample"
     )
   )
 
-  expect_equal(expected_plot, actual_plot)
+  expect_lists_equal_with_tolerance(expected_plot, actual_plot)
 })
 
 
 
 test_that("plot_serosurvey creates a binned ggplot with correct structure", {
-  data(veev2012)
+  actual_plot <- extract_plot_data(plot_serosurvey(serosurvey, bin_serosurvey = TRUE, bin_step = 10))
 
-  actual_serosurvey <- veev2012
-
-  actual_plot <- extract_plot_data(plot_serosurvey(actual_serosurvey, bin_serosurvey = TRUE, bin_step = 10))
-
-  expected_serosurvey <- create_expected_serosurvey(actual_serosurvey)
+  prepared_serosurvey <- create_prepared_serosurvey(serosurvey)
 
   expected_plot <- list(
     classes = c("gg", "ggplot"),
     layers = list(
       list(
         mapping = list(
-          ymin = "seroprev_lower",
-          ymax = "seroprev_upper"
+          ymin = "seroprev_lower", ymax = "seroprev_upper"
+        ),
+        geom_params = list(
+          na.rm = FALSE, orientation = NA, width = 0.1
         )
       ),
       list(
         mapping = list(
-          y = "seroprev",
-          size = "n_sample"
-        )
+          y = "seroprev", size = "n_sample"
+        ),
+        geom_params = list(na.rm = FALSE)
       )
     ),
     coordinates = list(
-      limits = list(
-        x = c(2, max(expected_serosurvey$age_max)),
-        y = c(min(expected_serosurvey$seroprev_lower), max(expected_serosurvey$seroprev_upper))
-      )
+      limits = list(x = c(2, 60), y = c(
+        0.3,
+        1
+      ))
     ),
     labels = list(
-      x = "Age",
-      y = "Seroprevalence",
-      ymin = "seroprev_lower",
-      ymax = "seroprev_upper",
-      size = "n_sample"
+      x = "Age", y = "Seroprevalence", ymin = "seroprev_lower",
+      ymax = "seroprev_upper", size = "n_sample"
     )
   )
 
-  expect_equal(expected_plot, actual_plot)
+  expect_lists_equal_with_tolerance(expected_plot, actual_plot)
+})
+
+test_that("plot_summary creates a ggplot with correct structure", {
+  loo_estimate_digits <- 1
+  central_estimate_digits <- 2
+  seroreversion_digits <- 2
+  rhat_digits <- 2
+  size_text <- 11
+
+  plot <- plot_summary(
+    seromodel = seromodel,
+    serosurvey = serosurvey,
+    loo_estimate_digits = loo_estimate_digits,
+    central_estimate_digits = central_estimate_digits,
+    rhat_digits = rhat_digits,
+    size_text = size_text
+  )
+  actual_data <- plot$data
+
+  actual_plot <- extract_plot_data(plot)
+
+  expected_data <- data.frame(
+    row = c(5, 4, 3, 2, 1),
+    text = c(
+      "model_name: constant_no_seroreversion",
+      "elpd_loo: -23.9(se=12.2)",
+      "foi: 0.082(95% CI, 0.063-0.1)",
+      "foi_rhat: 1",
+      "converged: yes"
+    )
+  )
+
+  expected_plot <- list(
+    classes = c("gg", "ggplot"),
+    layers = list(list(
+      mapping = list(
+        label = "text"
+      ),
+      geom_params = list(
+        parse = FALSE, check_overlap = FALSE,
+        size.unit = "mm", na.rm = FALSE
+      )
+    )),
+    coordinates = list(
+      limits = list(
+        x = NULL, y = NULL
+      )
+    ),
+    labels = list(x = "x", y = "row", label = "text")
+  )
+
+  # Checks that the actual plot data are at most 20% disimilar to the expected data
+  # using Levenshtein distance (adist)
+  expect_true({
+    all(map2_lgl(expected_data$text, actual_data$text, \(x, y) adist(x, y) / nchar(x) < 0.2))
+  })
+  expect_lists_equal_with_tolerance(expected_plot, actual_plot)
+})
+
+
+test_that("plot_seromodel creates a ggplot with correct structure", {
+  plot <- plot_seromodel(
+    seromodel = seromodel,
+    serosurvey = serosurvey
+  )
+  actual_plot <- extract_plot_data(plot)
+
+  expected_plot <- list(
+    classes = c("gg", "ggplot"),
+    layers = list(
+      list(
+        mapping = NULL,
+        geom_params = list(
+          xmin = 0, xmax = 1, ymin = 0.5, ymax = 1,
+          scale = 1, clip = "inherit", halign = 0.5, valign = 0.5
+        )
+      ),
+      list(mapping = NULL, geom_params = list(
+        xmin = 0, xmax = 1,
+        ymin = 0, ymax = 0.5, scale = 1, clip = "inherit", halign = 0.5,
+        valign = 0.5
+      ))
+    ),
+    coordinates = list(
+      limits = list(x = c(
+        0,
+        1
+      ), y = c(0, 1))
+    ),
+    labels = list()
+  )
+
+  expect_lists_equal_with_tolerance(expected_plot, actual_plot)
 })
