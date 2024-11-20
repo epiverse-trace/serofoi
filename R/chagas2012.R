351c75c9d63a5cb78f2693f27b51d99a5088a191
ntorresd <ntorresd@unal.edu.co>
2024-11-20 22:43:01 +0100

    doc: add documentation for undocumented datasets:   - `chagas2012’`   - `chik2015`   - `veev2012`

---
+++ R/chagas2012.R
@@ -0,0 +1,27 @@
+#' Chagas seroprevalence data in serofoi
+#'
+#' Datasets that measure the seroprevalence of IgG antibodies against
+#' Trypanosoma cruzi infection in rural areas of Colombia corresponding
+#' to a serosurvey conducted in 2012 for a rural indigenous community
+#' known to have long-term endemic transmission, where some control
+#' interventions have taken place over the years. 
+#'
+#' @docType data
+#'
+#' @usage data(chagas2012)
+#'
+#' @format ## `chagas2012`
+#' A `<data.frame>` with 4 rows and 5 columns:
+#' \describe{
+#'   \item{survey_year}{Year in which the serosurvey was conducted}
+#'   \item{n_sample}{Number of collected samples per age group}
+#'   \item{n_seropositive}{Number of positive samples per age group}
+#'   \item{age_min}{Age group minimal age}
+#'   \item{age_max}{Age group maximal age}
+#' }
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(chagas2012)
+"chagas2012"
