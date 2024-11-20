78cb8cb78f1e7c5c6d3b971a99bcde7f5805f9a7
ntorresd <ntorresd@unal.edu.co>
2024-11-20 22:43:01 +0100

    doc: add documentation for undocumented datasets:   - `chagas2012’`   - `chik2015`   - `veev2012`

---
+++ R/veev2012.R
@@ -0,0 +1,24 @@
+#' Venezuelan Equine Encephalitis Virus (VEEV) seroprevalence data in serofoi
+#'
+#' Datasets that measure the seroprevalence of IgG antibodies against VEEV in
+#' a rural village in Panamá in 2012. [carrera2020]
+#'
+#' @docType data
+#'
+#' @usage data(veev2012)
+#'
+#' @format ## `veev2012`
+#' A `<data.frame>` with 4 rows and 5 columns:
+#' \describe{
+#'   \item{survey_year}{Year in which the serosurvey was conducted}
+#'   \item{n_sample}{Number of collected samples per age group}
+#'   \item{n_seropositive}{Number of positive samples per age group}
+#'   \item{age_min}{Age group minimal age}
+#'   \item{age_max}{Age group maximal age}
+#' }
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(veev2012)
+"veev2012"
