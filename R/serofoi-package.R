07ebfa5cceef943e700935a4f16ff460713b2a68
ntorresd <ntorresd@unal.edu.co>
2024-11-24 20:29:58 +0100

    fix: remove unnecessary imports from package

--- R/serofoi-package.R
+++ R/serofoi-package.R
@@ -5,15 +5,10 @@
 #' @name serofoi-package
 #' @aliases serofoi
 #' @useDynLib serofoi, .registration = TRUE
-#' @import dplyr
-#' @importFrom dplyr %>%
 
 #' @import methods
 #' @import Rcpp
-#' @importFrom rstan sampling
-#' @importFrom graphics text
-#' @importFrom utils read.table
-#' @importFrom stats quantile
+#' @importFrom rstan sampling extract
 #' @importFrom rstantools rstan_config
 #' @references Stan Development Team (NA). RStan: the R interface to Stan. R
 #' package version 2.26.22. https://mc-stan.org
